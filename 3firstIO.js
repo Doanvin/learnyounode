fc4e7df9e1e7a42d06a1b0a5896168887b7a80bc
Donavin <donavinhannon@gmail.com>
2017-06-30 01:29:57 -0700

    Added EsLint

--- 3firstIO.js
+++ 3firstIO.js
@@ -5,17 +5,20 @@
 // The full path to the file to read will be provided as the first
 // command-line argument (i.e., process.argv[2]). You do not need to make
 // your own test file.
-var fs = require('fs');
-var newLines = 0;
+const fs = require('fs');
+
+const dirName = process.argv[2];
+let newLines = 0;
 
 fs.readFileSync(dirName);
-var dirName = process.argv[2];
-var buf = process.argv[3];
 
-str = buf.toString();
-strArr = str.split('\n');
+const buf = process.argv[3];
 
-for (i = 0; i < strArr.length; i++)
+const str = buf.toString();
+const strArr = str.split('\n');
+
+for (let i = 0; i < strArr.length; i++) {
   newLines += 1;
+}
 
 console.log(newLines);
